e94f28535d309d35a87fd6f6a8168084cda39c88
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-05-24 19:11:46 +0800

    created useHttp hook

---
+++ custom_hook_practice/src/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import useHttp from './hooks/use-http';
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  useHttp({
+    url: "https://reactcustomhook-e01fc-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+    method: get
+  })
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
