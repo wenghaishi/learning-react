750da0869476cc00e296cc727457b96be9d6cff5
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-06-03 20:15:50 +0800

    added feature to allow more than one item

--- advanced-redux/src/store/cart.js
+++ advanced-redux/src/store/cart.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-const initialCartState = { showCart: false, products: 0 };
+const initialCartState = { showCart: false, products: 0, items: [] };
 
 const cartSlice = createSlice({
   name: "cart",
@@ -8,11 +8,31 @@ const cartSlice = createSlice({
     toggleCart(state) {
       state.showCart = !state.showCart;
     },
-    addProduct(state) {
-      state.products++;
+    addProduct(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
     },
-    minusProduct(state) {
-      state.products--;
+    minusProduct(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
     },
   },
 });
